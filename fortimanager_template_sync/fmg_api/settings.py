2919eacdcdc68fed65a34de1064157d03471ed7a
Viktor Kertesz <viktor.kertesz@exxonmobil.com>
2023-09-15 11:33:04 +0200

    fmgapi - satisfy pylint about pydantic annotation

--- fortimanager_template_sync/fmg_api/settings.py
+++ fortimanager_template_sync/fmg_api/settings.py
@@ -2,16 +2,19 @@
 from pydantic import Field, SecretStr, field_validator
 from pydantic.networks import HttpUrl
 from pydantic_settings import BaseSettings
+from typing_extensions import Annotated
 
 
 class FMGSettings(BaseSettings):
     """Fortimanager settings"""
 
-    base_url: HttpUrl = Field(..., description="Base URL to access FMG (e.g.: https://myfmg/jsonrpc)")
-    username: str = Field(..., description="User to authenticate")
-    password: SecretStr = Field(..., description="Password for authentication")
-    adom: str = Field("Global", description="ADOM to use for this connection")
-    verify: bool = Field(True, description="Verify SSL certificate (REQUESTS_CA_BUNDLE can set accepted CA cert)")
+    base_url: Annotated[HttpUrl, Field(description="Base URL to access FMG (e.g.: https://myfmg/jsonrpc)")]
+    username: Annotated[str, Field(description="User to authenticate")]
+    password: Annotated[SecretStr, Field(description="Password for authentication")]
+    adom: Annotated[str, Field(description="ADOM to use for this connection")] = "Global"
+    verify: Annotated[
+        bool, Field(description="Verify SSL certificate (REQUESTS_CA_BUNDLE can set accepted CA cert)")
+    ] = True
 
     @field_validator("base_url", mode="before")
     @classmethod
