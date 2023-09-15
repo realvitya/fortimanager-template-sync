94e761b8702f33aeae78125b6f5723adbd95c42d
Viktor Kertesz <viktor.kertesz@exxonmobil.com>
2023-09-15 11:31:59 +0200

    fmgapi - better connection handling

--- fortimanager_template_sync/fmg_api/connection.py
+++ fortimanager_template_sync/fmg_api/connection.py
@@ -1,7 +1,7 @@
 """FMG connection"""
 import functools
 import logging
-from typing import Optional
+from typing import Any, Optional
 
 import requests
 from pydantic import SecretStr
@@ -14,6 +14,7 @@ logger = logging.getLogger(__name__)
 
 def auth_required(func):
     """decorator to provide authentication for the method"""
+
     @functools.wraps(func)
     def decorated(self, *args, **kwargs):
         """method which needs authentication"""
@@ -22,7 +23,11 @@ def auth_required(func):
         try:
             return func(self, *args, **kwargs)
         except FMGException as err:
-            raise err
+            try:  # try again after refreshing token
+                self._token = self._get_token()  # pylint: disable=protected-access  # decorator of methods
+                return func(self, *args, **kwargs)
+            except FMGException as err2:
+                raise err2 from err
 
     return decorated
 
@@ -34,15 +39,32 @@ class FMG:
         logger.debug("Initializing connection to %s", settings.base_url)
         self._settings = settings
         self._token: Optional[SecretStr] = None
-        self._session = requests.Session()
+        self._session: Optional[requests.Session] = None
 
     def open(self):
         """open connection"""
+        self._session = requests.Session()
         self._token = self._get_token()
 
     def close(self):
         """close connection"""
+        # Logout and expire token
+        request = {
+            "id": 1,
+            "method": "exec",
+            "params": [{"url": "/sys/logout"}],
+            "session": self._token.get_secret_value(),
+        }
+        try:
+            req = self._session.post(self._settings.base_url, json=request, verify=self._settings.verify)
+            status = req.json().get("result", [{}])[0].get("status", {})
+            if status.get("code") != 0:
+                logger.warning("Logout failed!")
+        except requests.exceptions.ConnectionError:
+            logger.warning("Logout failed!")
+
         self._session.close()
+        logger.debug("Closed session")
 
     def __enter__(self):
         self.open()
@@ -50,6 +72,13 @@ class FMG:
 
     def __exit__(self, exc_type, exc_value, exc_tb):
         self.close()
+
+    def _post(self, request: dict) -> Any:
+        req = self._session.post(self._settings.base_url, json=request, verify=self._settings.verify)
+        results = req.json().get("result", [])
+        if any(status := result["status"] for result in results if result["status"]["code"] != 0):
+            raise FMGException(status)
+        return results[0] if len(results) == 1 else results
 
     def _get_token(self) -> SecretStr:
         """Get authentication token
@@ -80,7 +109,7 @@ class FMG:
         except requests.exceptions.ConnectionError as err:
             logger.error("Can't gather token: %s", err)
             raise err
-        token = req.json().get("session")
+        token = req.json().get("session", "")
         return SecretStr(token)
 
     @auth_required
@@ -92,5 +121,5 @@ class FMG:
             "id": 1,
             "session": self._token.get_secret_value(),
         }
-        req = self._session.post(self._settings.base_url, json=request, verify=self._settings.verify)
-        return req.json().get("result")[0].get("data").get("Version")
+        req = self._post(request)
+        return req["data"]["Version"]
