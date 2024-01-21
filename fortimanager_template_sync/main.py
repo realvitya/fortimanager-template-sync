23ab78696eaa60a8c7d64a720c2430fa0032485c
Viktor Kertesz <viktor.kertesz@exxonmobil.com>
2024-01-21 12:58:41 +0100

    added GIT handling
    started working on the sync steps

--- fortimanager_template_sync/main.py
+++ fortimanager_template_sync/main.py
@@ -2,12 +2,15 @@
 import logging
 import time
 from logging.config import dictConfig
+from pathlib import Path
 
+import dotenv
 import typer
 from typing_extensions import Annotated
 
 from fortimanager_template_sync import __version__
 from fortimanager_template_sync.config import FMGSyncSettings
+from fortimanager_template_sync.task import FMGSyncTask
 
 app = typer.Typer(context_settings={"help_option_names": ["-h", "--help"]}, add_completion=False)
 logger = logging.getLogger("fortimanager_template_sync.main")
@@ -15,6 +18,23 @@ logger = logging.getLogger("fortimanager_template_sync.main")
 
 @app.command()
 def run(
+    template_repo: Annotated[
+        str, typer.Option("--template-repo", "-t", envvar="FMGSYNC_TEMPLATE_REPO", help="Template repository URL")
+    ] = None,
+    template_branch: Annotated[
+        str, typer.Option("--template-branch", "-b", envvar="FMGSYNC_TEMPLATE_BRANCH",
+                          help="Branch in repository to sync")
+    ] = "main",
+    git_token: Annotated[str, typer.Option("--git-token", envvar="FMGSYNC_GIT_TOKEN")] = None,
+    local_repo: Annotated[Path, typer.Option("--local-path", "-l", envvar="FMGSYNC_LOCAL_PATH")] = "./fmg-templates/",
+    fmg_url: Annotated[str, typer.Option("--fmg-url", "-url", envvar="FMGSYNC_FMG_URL")] = None,
+    fmg_user: Annotated[str, typer.Option("--fmg-user", "-u", envvar="FMGSYNC_FMG_USER")] = None,
+    fmg_pass: Annotated[str, typer.Option("--fmg-pass", "-p", envvar="FMGSYNC_FMG_PASS")] = None,
+    fmg_adom: Annotated[str, typer.Option("--fmg-adom", "-a", envvar="FMGSYNC_FMG_ADOM")] = "root",
+    protected_fw_group: Annotated[
+        str, typer.Option("--protected-firewall-group", "-pg", envvar="FMGSYNC_PROTECTED_FW_GROUP",
+                          help="This group in FMG will be checked for FW status. Also this group will be deployed only")
+    ] = "production",
     version: Annotated[bool, typer.Option("--version", "-V", help="print version")] = False,
     prod_run: Annotated[bool, typer.Option("--force-changes", "-f", help="do changes")] = False,
     debug: Annotated[int, typer.Option("--debug", "-D", help="debug logs", count=True)] = 0,
@@ -28,6 +48,15 @@ def run(
         return
 
     settings = FMGSyncSettings(
+        template_repo=template_repo,
+        template_branch=template_branch,
+        git_token=git_token,
+        local_repo=local_repo,
+        fmg_url=fmg_url,
+        fmg_user=fmg_user,
+        fmg_pass=fmg_pass,
+        fmg_adom=fmg_adom,
+        protected_fw_group=protected_fw_group,
         prod_run=prod_run,
         debug=debug,
         logging_config=logging_config,
@@ -41,12 +70,14 @@ def run(
         logging.level = logging.DEBUG
 
     start_time = time.time()
-    print(settings)
+    job = FMGSyncTask(settings)
+    job.run()
     logger.info("Operation took %ss", round(time.time() - start_time, 2))
 
 
 def main():
     """Typer runner"""
+    dotenv.load_dotenv("fmgsync.env")
     app()
 
 
--- fortimanager_template_sync/task.py
+++ fortimanager_template_sync/task.py
@@ -1,5 +1,23 @@
 """FMG Sync Task"""
+import logging
+import re
+from pathlib import Path
+from typing import Optional, List
+
+from git import Repo, InvalidGitRepositoryError, GitCommandError
+
 from fortimanager_template_sync.config import FMGSyncSettings
+from fortimanager_template_sync.fmg_api.data import CLITemplate, CLITemplateGroup
+
+
+logger = logging.getLogger("fortimanager_template_sync.task")
+
+
+class TemplateTree:
+    """Template data structure"""
+    prerun_templates: List[CLITemplate]
+    templates: List[CLITemplate]
+    template_groups: List[CLITemplateGroup]
 
 
 class FMGSyncTask:
@@ -9,14 +27,14 @@ class FMGSyncTask:
     Steps of this task:
     1. update local repository from remote
     2. check if there was a change
-    3. check FMG device status list
+    3. check FMG device status list in protected group
        If firewalls are not in sync, stop
     4. download FMG templates and template groups from FMG
     5. build list of templates to delete from FMG
     6. build list of templates to upload to FMG
     7. execute changes in FMG
     8. check firewall statuses
-    9. deploy changes to firewalls
+    9. deploy changes to firewalls in protected group only
     10. check firewall statuses again
 
     Attributes:
@@ -37,3 +55,93 @@ class FMGSyncTask:
         Raises:
             FMGSyncException
         """
+        # 1. update local repository from remote
+        repo = self._update_local_repository()
+        # 2. load data from the repo
+        repo_data = self._load_local_repository()
+        # 3. check FMG device status list in protected group
+        #    If firewalls are not in sync, stop
+        # 4. download FMG templates and template groups from FMG
+        # 5. build list of templates to delete from FMG
+        # 6. build list of templates to upload to FMG
+        # 7. execute changes in FMG
+        # 8. check firewall statuses
+        # 9. deploy changes to firewalls in protected group only
+        # 10. check firewall statuses again
+
+    def _update_local_repository(self) -> Optional[Repo]:
+        """Clone or update local repository
+
+        Repository is expected in this format:
+
+        ```
+        pre-run/
+            prerun1.j2
+            prerun2.j2
+            ...
+        templates/
+            template1.j2
+            template2.j2
+            ...
+        template-groups/
+            group1.j2
+            group2.j2
+            ...
+        ```
+
+        Any other files are ignored. All directories are optional, it's not mandatory to have all of them.
+        See [docs](repository.md) for additional information.
+
+        Returns:
+            Repository with cloned templates
+        """
+        logger.info("Checking out template repository")
+        try:
+            repo = Repo(self.settings.local_repo)
+            repo.git.pull()  # download updates
+            repo.git.checkout(self.settings.template_branch)
+            return repo
+        except InvalidGitRepositoryError:  # in case of an empty directory
+            logger.info("Cloning template repository")
+            repo = Repo.clone_from(url=self.settings.template_repo,
+                                   to_path=self.settings.local_repo,
+                                   branch=self.settings.template_branch)
+            return repo
+        except GitCommandError:
+            logger.error("Can't checkout repo: '%s' branch: '%s'",
+                         self.settings.template_repo, self.settings.template_branch)
+            raise
+
+    def _load_local_repository(self) -> TemplateTree:
+        """Load files from repository"""
+        template_path = Path(self.settings.local_repo) / "templates"
+        templates = []
+        if template_path.is_dir():
+            for template_file in template_path.glob("*.j2"):
+                with open(template_file) as fi:
+                    data = fi.read()
+                    parsed_data = self._parse_template_file(template_file.name.replace("*.j2", ""), data)
+                    templates.append(parsed_data)
+
+    def _parse_template_file(self, name: str, data: str) -> CLITemplate:
+        """Parse template script text
+
+        Expected format for metadata (head comment):
+
+
+        Args:
+            name (str): name of the template (file name without extension)
+            data (str): raw text of the script file
+        """
+        description = ""
+        variables = []
+        # cut header comment
+        match = re.match(r"^{#(.*?)#}", data, re.S)
+        if match:
+            header = match.group(1)
+            match = re.match(r"{#(.*)", header)
+            description = match.group(1)
+
+        return CLITemplate(name=name,
+                           description=description,
+                           variables=variables)
