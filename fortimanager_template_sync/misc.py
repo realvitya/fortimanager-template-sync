8c657d1cc170476a2ef670a9a4710b4d3a0ab540
Viktor Kertesz <vkertesz2@gmail.com>
2024-01-21 19:22:23 +0100

    added docstring to misc

--- fortimanager_template_sync/misc.py
+++ fortimanager_template_sync/misc.py
@@ -1,7 +1,17 @@
+"""Miscellaneous utilities."""
 from jinja2 import Environment, meta
 
 
-def find_all_vars(template_content):
+def find_all_vars(template_content: str) -> set:
+    """
+    Find all undeclared variables in the given template content.
+
+    Args:
+        template_content (str): The content of the template.
+
+    Returns:
+        list: A list of undeclared variables found in the template content.
+    """
     env = Environment()
     parsed_content = env.parse(template_content)
 
